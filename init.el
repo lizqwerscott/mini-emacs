2bf68cb29554d2f403243facb20f37fd6a86d591
lizqwerscott <lizqwerscott@163.com>
2025-02-15 10:41:54 +0800

    feat: config icomplete

--- init.el
+++ init.el
@@ -16,6 +16,22 @@
       completion-category-defaults nil
       completion-category-overrides '((file (styles basic partial-completion))))
 
+(setq tab-always-indent 'complete
+      icomplete-delay-completions-threshold 0
+      icomplete-compute-delay 0
+      icomplete-show-matches-on-no-input t
+      icomplete-hide-common-prefix nil
+      icomplete-prospects-height 9
+      icomplete-separator " . "
+      icomplete-with-completion-tables t
+      icomplete-in-buffer t
+      icomplete-max-delay-chars 0
+      icomplete-scroll t
+      resize-mini-windows 'grow-only
+      icomplete-matches-format nil)
+(bind-key "TAB" #'icomplete-force-complete icomplete-minibuffer-map)
+(bind-key "RET" #'icomplete-force-complete-and-exit icomplete-minibuffer-map)
+
 (with-eval-after-load 'fussy
   (fussy-setup))
 
