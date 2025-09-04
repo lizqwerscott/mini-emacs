e44d49bfb4a0df69389eeabd7950b992498430d4
lizqwerscott <lizqwerscott@163.com>
2025-09-04 22:16:46 +0800

    feat: remove obsolete find-function-setup-keys

--- early-init.el
+++ early-init.el
@@ -5,7 +5,6 @@
 ;; 增加IO性能
 (setq read-process-output-max (* 1024 1024 10))
 (setq gc-cons-threshold most-positive-fixnum)
-(find-function-setup-keys)
 
 ;; Avoid raising the *Messages* buffer if anything is still without
 ;; lexical bindings
