3cbef837916b1d3968a83ca45e7a57090dc808d6
lizqwerscott <lizqwerscott@163.com>
2025-09-12 19:03:08 +0800

    feat: use ustc mirror

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -12,9 +12,6 @@
 (require 'package)
 (require 'cl-lib)
 
-;; (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-;;                          ("nongnu" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
-;;                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (setq package-archives '(("gnu" . "https://mirrors.ustc.edu.cn/elpa/gnu/")
                          ("melpa" . "https://mirrors.ustc.edu.cn/elpa/melpa/")
                          ("nongnu" . "https://mirrors.ustc.edu.cn/elpa/nongnu/")))
